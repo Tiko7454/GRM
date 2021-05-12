340a30aaca856aec009abb12fcbd43c3a50ee3e6
Tiko7454 <tiko7454av@gmail.com>
2021-05-13 00:15:30 +0400

    Update GRM.c
    added grm install destination
    bug fixed in is_link function

--- root/GRM.c
+++ root/GRM.c
@@ -1,30 +1,22 @@
-// C program to create loading bar
 #include <stdio.h>
 #include <string.h>
-#include <windows.h>
+#include <stdlib.h>
 
-// Function to creating loading bar
+
 void loadingBar(){
-  
-    // Initialize char for printing
-    // loading bar
     char a = '.', b = '*';
     printf("[");
   
-    // Print initial loading bar
     for (int i = 0; i < 26; i++)
         printf("%c", a);
     printf("]");
   
-    // Set the cursor again starting
-    // point of loading bar
     printf("\r");
     printf("[");
-    // Print loading bar progress
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < 26; i++)
         printf("%c", b);
-    }
 }
+
 
 int is_link(char* x){
     if(strlen(x) < 8)
@@ -32,19 +24,32 @@ int is_link(char* x){
     char prefix[9];
     for(int i = 0; i < 8; i++)
         prefix[i] = x[i];
+    prefix[8] = 0;
     return strcmp(prefix, "https://") == 0;
 }
 
-// Syntax
 // grm install ...
 int main(int argc, char** argv){
-    char github_prefix[100] = "https://github.com/"
-	char clone[100] = "git clone ";
-	char* pull = "git pull";
-	char* mode = argv[1];
-	if(strcmp(mode, "install") == 0){
-    	strcat(clone, argv[2]);
-    	system(clone);
-	}
-    return 0;
+    char github_prefix[100] = "https://github.com/";
+    char clone[100] = "git clone ";
+    char* pull = "git pull";
+    char* mode = argv[1];
+    if(strcmp(mode, "install") == 0){
+        for(int i = 2; i < argc; i++){
+            char* arg = argv[i];  // add default github prefix if necessary
+            strcat(clone, arg);
+            strcat(clone, " ../RPs/");
+            // unlinking arg
+                char unlinked_arg[100] = "";
+                if(is_link(arg))
+                    for(int i = 8; i < strlen(arg); i++)
+                        unlinked_arg[i-8] = arg[i];
+                else
+                    strcat(unlinked_arg, arg);
+            //end
+            strcat(clone, unlinked_arg);
+            system(clone);
+        }
+    }
+
 }
